a9749f3dd8056b7ceb982d951f3d07d9c2dea26f
masTech Developer <ashir342@gmail.com>
2020-06-08 00:25:54 +0500

    update app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-// import Navbar from './conponents/layout/navbar';
+import Navbar from './conponents/layout/navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1>Hello World! Auto Deploy</h1>
-      </header>
-    </div>
+    <Navbar />
+
+    // <div className='App'>
+    //   <header className='App-header'>
+    //     <img src={logo} className='App-logo' alt='logo' />
+    //     <h1>Hello World! Auto Deploy</h1>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/conponents/layout/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  render(
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default Navbar;
