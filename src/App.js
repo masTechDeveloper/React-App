155322c8c0dc4389dfefd6ab9b949a4ec7ea6c5b
masTech Developer <ashir342@gmail.com>
2020-06-08 00:29:14 +0500

    App.js Update

--- src/App.js
+++ src/App.js
@@ -3,16 +3,7 @@ import Navbar from './conponents/layout/navbar';
 import './App.css';
 
 function App() {
-  return (
-    <Navbar />
-
-    // <div className='App'>
-    //   <header className='App-header'>
-    //     <img src={logo} className='App-logo' alt='logo' />
-    //     <h1>Hello World! Auto Deploy</h1>
-    //   </header>
-    // </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
--- src/conponents/layout/navbar.js
+++ src/conponents/layout/navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Navbar() {
-  render(
+  return (
     <div>
       <h1>Hello World</h1>
     </div>
